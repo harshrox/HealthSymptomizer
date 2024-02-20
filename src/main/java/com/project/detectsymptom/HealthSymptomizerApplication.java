2e2b23c421fa38daa8efc224d1fa70fb20e73327
harsh <harshjha08@outlook.com>
2024-02-20 22:58:53 +0530

    Initializing

---
+++ src/main/java/com/project/detectsymptom/HealthSymptomizerApplication.java
@@ -0,0 +1,13 @@
+package com.project.detectsymptom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthSymptomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthSymptomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/detectsymptom/HealthSymptomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.detectsymptom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSymptomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
